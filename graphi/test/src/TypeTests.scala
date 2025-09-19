ca9311295cf52cc5fb06d794455566ab8a47b131
Chris Oates <chrispoates@gmail.com>
2025-09-19 17:58:38 +0100

    Added tests for directed cycle graph.

--- graphi/test/src/TypeTests.scala
+++ graphi/test/src/TypeTests.scala
@@ -2,7 +2,7 @@ package graphi.test
 
 import utest.*
 
-import graphi.SimpleMapGraph
+import graphi.{DirectedMapGraph, SimpleMapGraph}
 import graphi.algorithms.GraphType.{isNullGraph, isTrivial, isCycleGraph}
 
 object TypeTests extends TestSuite {
@@ -23,7 +23,7 @@ object TypeTests extends TestSuite {
 			val g4 = g2.addEdge("A", "A") // self-loop not currently allowed
 			assert(isTrivial(g4))
 		}
-		test("isCycleGraph") {
+		test("isCycleGraphSimple") {
 			val g = new SimpleMapGraph[String]()
 			assert(!isCycleGraph(g))
 			val g2 = g.addNode("A")
@@ -38,6 +38,31 @@ object TypeTests extends TestSuite {
 			assert(!isCycleGraph(g6))
 
 			val g7 = new SimpleMapGraph[Int]()
+				.addNode(1)
+				.addNode(2)
+				.addNode(3)
+				.addNode(4)
+				.addEdge(1, 2)
+				.addEdge(2, 3)
+				.addEdge(3, 4)
+				.addEdge(4, 1)
+			assert(isCycleGraph(g7))
+		}
+		test("isCycleGraphDirected") {
+			val g = new DirectedMapGraph[String]()
+			assert(!isCycleGraph(g))
+			val g2 = g.addNode("A")
+			assert(!isCycleGraph(g2))
+			val g3 = g2.addNode("B").addNode("C")
+			assert(!isCycleGraph(g3))
+			val g4 = g3.addEdge("A", "B").addEdge("B", "C").addEdge("C", "A")
+			assert(isCycleGraph(g4))
+			val g5 = g4.addNode("D")
+			assert(!isCycleGraph(g5))
+			val g6 = g5.addEdge("C", "D").addEdge("D", "A")
+			assert(!isCycleGraph(g6))
+
+			val g7 = new DirectedMapGraph[Int]()
 				.addNode(1)
 				.addNode(2)
 				.addNode(3)
