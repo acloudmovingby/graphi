5d5c81626aa4015b5ebc3defcc8312a3e86efe4c
Chris Oates <chrispoates@gmail.com>
2025-11-23 20:51:27 -0500

    Add removeEdge function

--- graphi/src/DirectedMapGraph.scala
+++ graphi/src/DirectedMapGraph.scala
@@ -6,7 +6,7 @@ package graphi
  * @param adjMap The adjacency map representing the graph
  * @tparam A The type of the nodes in the graph
  */
-class DirectedMapGraph[A](val adjMap: Map[A, Set[A]] = Map.empty[A, Set[A]]) extends graphi.MapGraph[DirectedMapGraph[A], A] {
+class DirectedMapGraph[A](val adjMap: Map[A, Set[A]] = Map.empty[A, Set[A]]) extends graphi.MapGraph[A, DirectedMapGraph[A]] {
 	// boilerplate to help inheritance + immutability work correctly, probably should be using typeclasses somehow but couldn't figure it out
 	override protected def returnThis: DirectedMapGraph[A] = this
 
@@ -54,5 +54,18 @@ class DirectedMapGraph[A](val adjMap: Map[A, Set[A]] = Map.empty[A, Set[A]]) ext
 		if (!adjMap.contains(node)) throw new NoSuchElementException(s"The node $node doesn't exist")
 		adjMap.filter { case (_, neighbors) => neighbors.contains(node) }.keys.toSet
 	}
-}
 
+	/**
+	 * Removes the edge from 'from' to 'to'.
+	 * Only removes the edge in the directed direction.
+	 * Throws NoSuchElementException if either node doesn't exist.
+	 * Returns a new graph with the edge removed.
+	 */
+	override def removeEdge(from: A, to: A): DirectedMapGraph[A] = {
+		if (!adjMap.contains(from)) throw new NoSuchElementException(s"The node $from doesn't exist")
+		if (!adjMap.contains(to)) throw new NoSuchElementException(s"The node $to doesn't exist")
+		val updatedNeighbors = adjMap(from) - to
+		val updatedAdjMap = adjMap + (from -> updatedNeighbors)
+		constructNewThis(updatedAdjMap)
+	}
+}
